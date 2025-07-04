9bb4633d580113cde001236acc99cf33f9af6683
Jhonmcr <jhoncariaco@gmai.com>
2025-07-03 20:18:36 -0400

    Con este commit ya estamos en render pero hay varios errores 2.0

--- frontend_dist/scripts/home/counter_loader.js
+++ frontend_dist/scripts/home/counter_loader.js
@@ -29,11 +29,11 @@ async function loadCasesCounters() {
         // Realiza una petición fetch para obtener todos los casos.
         // Se usa un límite alto para asegurar que se obtengan todos los casos para el conteo.
         // en lugar de traer todos los datos de los casos al frontend para contarlos aquí.
-        const response = await fetch(`${API_BASE_URL}/casos`); 
+        const response = await fetch(`${API_BASE_URL}/casos?limit=10000`); 
         // Verifica si la respuesta de la petición fue exitosa.
         if (!response.ok) {
             // Si no fue exitosa, lanza un error con el estado HTTP.
-            throw new Error(`Error HTTP! estado: ${response.status}`);
+            throw new Error(`Error HTTP! estado: ${response.status}`); 
         }
         // Convierte la respuesta a formato JSON.
         const responseData = await response.json(); 
