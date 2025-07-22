2935d3e9aba1f5379ac03d2ec414133b40a542f6
Jhonmcr <jhoncariaco@gmai.com>
2025-07-21 22:09:17 -0400

    Actualizacion hay varios errores que en la parte de otc.html que estamos solucionando 4.0

--- frontend/scripts/otc.js
+++ frontend/scripts/otc.js
@@ -84,7 +84,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                         comunaDiv.innerHTML = `
                             <span>${comuna.nombre} (${comuna.consejos_comunales.length} Consejos Comunales)</span>
                             <button class="modify-comuna-btn" data-id="${comuna._id}" data-nombre="${comuna.nombre}" data-codigo="${comuna.codigo_circuito_comunal}">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <svg class="modify-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                                 </svg>
                             </button>
@@ -414,7 +414,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                 consejoDiv.innerHTML = `
                     <span>${consejo.nombre} (${consejo.codigo_situr})</span>
                     <button class="modify-consejo-btn" data-id="${consejo._id}" data-nombre="${consejo.nombre}" data-codigo="${consejo.codigo_situr}">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <svg class="modify-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                         </svg>
                     </button>
